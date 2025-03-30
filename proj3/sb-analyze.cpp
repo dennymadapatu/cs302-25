29d3edcd07b4bddb6b6101e70bf5f87b0d4ee676
dennymadapatu <dennismadapatu@gmail.com>
2025-03-30 17:13:16 -0400

    Update sb-analyze.cpp

--- proj3/sb-analyze.cpp
+++ proj3/sb-analyze.cpp
@@ -1,7 +1,12 @@
+// Name: Dennis Madapatu
+// Date: 2/21/2025
+// Description: Superball Analysis. Analyzes a given board of superball and gives the scoreable sets along with the goal cell to score them through
 #include <cstdio>
 #include <cstring>
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <set>
 
 #include "disjoint.h"
 
@@ -77,14 +82,80 @@ Superball::Superball(int argc, char **argv)
   }
 }
 
+void sb_analyze(Superball *s) {
+  DisjointSetByRankWPC ds(s->r*s->c);
+  unordered_map<int, set<int>> heads;
+  set<int> headIndices;
+  int row = s->r;
+  int col = s->c;
+  for(int i = 0; i < row; i++) {
+    for(int j = 0; j < col; j++) {
+      int index = i * col + j;
+      if(s->board[index] == '.' || s->board[index] == '*') { // Skip all blanks
+        continue;
+      }
+      if(s->goals[index] == 1 && s->mss == 1) { // An edge case where minimum scoring size is 1
+        headIndices.insert(index);
+        heads[index].insert(index);
+      }
+      if(i < row - 1) { // Checking under, as long as we aren't on the last row
+        if(s->board[index] == s->board[(i + 1) * col + j]) {
+          int newIndex = ds.Union(ds.Find(index), ds.Find((i + 1) * col + j));
+          if(newIndex == -1) { // Both indices are the same, move on
+            continue;
+          }
+          // Insert the attached cell and the head index for later reference
+          heads[ds.Find(newIndex)].insert(index);
+          headIndices.insert(ds.Find(newIndex));
+        }
+      }
+      if(s->board[index] == s->board[index + 1]) { // checks to the right
+        if((index + 1) % col == 0) { // We reached a new row, doesn't count
+          continue;
+        }
+        int newIndex = ds.Union(ds.Find(index), ds.Find(index + 1));
+        if(newIndex == -1) {
+          continue;
+        }
+        heads[ds.Find(newIndex)].insert(index);
+        headIndices.insert(ds.Find(newIndex));
+      }
+    }
+  }
+  // Using this to erase any smaller sets inside of larger sets, and inserting all the values in those sets into the larger set
+  for(int i = 0; i < row; i++) {
+    for(int j = 0; j < col; j++) {
+      int index = i * col + j;
+      if(ds.Find(index) != index) {
+        heads.erase(index);
+        heads[ds.Find(index)].insert(index);
+        heads[ds.Find(index)].insert(ds.Find(index));
+      }
+    }
+  }
+  // Print formatting for the analyze
+  set<int> scoredSets; // Made so the program doesn't print out sets that have already been scored
+  cout << "Scoring sets:" << endl;
+  for(int val : headIndices) {
+    if((int) heads[val].size() >= s->mss) {
+      for(int index : heads[val]) {
+        if(s->goals[index] == 1) {
+          if(scoredSets.find(ds.Find(index)) == scoredSets.end()) {
+            int columnIndex = index % col;
+            int rowIndex = index / col;
+            cout << "  Size: " << heads[val].size() << "  Char: " << (char) s->board[val]
+                 << "  Scoring Cell: " << rowIndex << "," << columnIndex << "\n";
+            scoredSets.insert(ds.Find(index));
+          }
+        }
+      }
+    }
+  }
+}
+
 int main(int argc, char **argv)
 {
   Superball *s;
- 
   s = new Superball(argc, argv);
-
-  DisjointSetByRankWPC ds(s->r*s->c);
-
-  ds.Print();
-  
+  sb_analyze(s);
 }
